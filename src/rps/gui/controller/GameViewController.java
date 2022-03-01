1abdb68f2d70f159ab78e97af5122f7e1526b2f0
silamin <amine.aouina.lp.4@gmail.com>
2022-03-01 12:33:22 +0100

    workInProgress

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,19 +1,25 @@
 package rps.gui.controller;
 
 // Java imports
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+
+import java.awt.*;
+import java.awt.image.*;
+import javafx.scene.image.Image;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import rps.bll.game.*;
 import rps.bll.player.Player;
 import rps.bll.player.PlayerType;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,6 +29,7 @@ import java.util.ResourceBundle;
  */
 public class GameViewController implements Initializable {
 
+    public AnchorPane anchorPane;
     @FXML
     private Label roundLabel,tieScore,botScore,humanScore;;
     @FXML
@@ -33,6 +40,7 @@ public class GameViewController implements Initializable {
     private GameManager gameManager;
     private Player human;
     private Player bot;
+    private final ImageIcon scissorSiLamin= new ImageIcon(("scissorSiLamin.png"));
 
     public GameViewController(){
         human = new Player("Renars", PlayerType.Human);
@@ -42,6 +50,16 @@ public class GameViewController implements Initializable {
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        HBox scissorBox= new HBox();
+        /*try {
+            resize("/Users/aminaouina/Documents/GitHub/rps2022/Untitled/Resources/scissorSiLamin.png","/Users/aminaouina/Documents/GitHub/rps2022/Untitled/Resources/resizedScissor.png",40,30);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        for (int i =1;i<=3;i++)
+        scissorBox.getChildren().add(new ImageView(new Image("resizedScissor.png")));
+        anchorPane.getChildren().add(scissorBox);
         rockImage.setImage(new Image("rock.png"));
         paperImage.setImage(new Image("paper.png"));
         scissorImage.setImage(new Image("scissor.png"));
@@ -84,8 +102,28 @@ public class GameViewController implements Initializable {
             botScore.setText(String.valueOf(Integer.valueOf(botScore.getText())+1));
         else
             humanScore.setText(String.valueOf(Integer.valueOf(botScore.getText())+1));
-
     }
+    public void resize(String inputImagePath,
+                       String outputImagePath, int scaledWidth, int scaledHeight)
+            throws IOException {
+        // reads input image
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
 
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
 
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+        g2d.dispose();
+
+        // extracts extension of output file
+        String formatName = outputImagePath.substring(outputImagePath
+                .lastIndexOf(".") + 1);
+
+        // writes to output file
+        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+    }
 }
